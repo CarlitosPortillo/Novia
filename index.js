0feb6717e0207cba603adcaae35fcfb80c40589f
CarlitosPortillo <94661833+CarlitosPortillo@users.noreply.github.com>
2021-11-25 18:21:01 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const yesBtn = document.querySelector('#yesBtn');
 
 yesBtn.addEventListener('click',function () {
-    alert('Nos Caimos y nos volvimos a levantar <3');
+    alert('Ni queria xd ');
 });
 
 const noBtn = document.querySelector('#noBtn');
